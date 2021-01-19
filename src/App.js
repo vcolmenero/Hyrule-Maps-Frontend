b4d36de3bc20f3a0e584308d58909b9e107db4b1
Victor Colmenero <vcolmenero90@gmail.com>
2021-01-18 18:09:20 -0800

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
 /* eslint-disable no-unused-vars */
-
-import React, { useState } from 'react';
-import { locations } from './locations';
-import { fetchMe } from './fetching'
+import {useEffect, useState} from 'react';
 import './App.css';
-import {render} from 'react';
-import userEvent from '@testing-library/user-event';
-
-
-const App = () => {
-  const [castleTown, setCastleTown] = useState("hidden")
-  const [mountain, setMountain] = useState("hidden")
-  const [tree, setTree] = useState("hidden")
-  const [field, setField] = useState("hidden")
-  const [gerudo, setGerudo] = useState("hidden")
-  const [castle, setCastle] = useState("hiiden")
-  const [kakariko, setKakariko] = useState("hidden")
-  const [kokiri, setKakiri] = useState("hidden")
-  const [lake, setLake] = useState("hidden")
-  const [ranch, setRanch] = useState("hidden")
-  const [domain, setDomain] = useState("hidden")
-  const [currentLocation, setCurrentLocation] = useState("hidden")
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import DashboardPage from './pages/DashboardPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {Route, Switch} from 'react-router-dom';
+import { locations } from './locations';
+import { getUser, userState, logout } from './services/userService';
+import { withRouter, Redirect } from 'react-router-dom'
 
 
 
 
-  
+
+function App() {
+  const [castleTown, setCastleTown] = useState("hidden");
+  const [mountain, setMountain] = useState("hidden");
+  const [tree, setTree] = useState("hidden");
+  const [field, setField] = useState("hidden");
+  const [gerudo, setGerudo] = useState("hidden");
+  const [castle, setCastle] = useState("hiiden");
+  const [kakariko, setKakariko] = useState("hidden");
+  const [kokiri, setKakiri] = useState("hidden");
+  const [lake, setLake] = useState("hidden");
+  const [ranch, setRanch] = useState("hidden");
+  const [domain, setDomain] = useState("hidden");
+  const [currentLocation, setCurrentLocation] = useState("hidden");
+
+
+
+
+
   const mapImages = Object.entries(locations).map(([id, location]) => (
     <img
       key={id}
       id={id}
       src={location.image}
       alt="Hyrule map"
-      className={`map-item ${location.name}`}
-    />
+      className={`map-item ${location.name}`} />
   ));
 
-  
+
   const toggleCurrentLocationId = (id) => {
-    const locationId = currentLocation === id ? "" : id; 
+    const locationId = currentLocation === id ? "" : id;
     setCurrentLocation(locationId);
   };
 
-  
+
   const listLocations = Object.entries(locations).map(([id, location]) => (
     <p key={id} onClick={() => toggleCurrentLocationId(id)}>
       {location.name}
@@ -55,59 +62,59 @@ const App = () => {
       <div className="main">
         <div className="left-section">
           <div className="map-container">
-            <img src={require('./images/castleTown.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${castleTown}`} 
+            <img src={require('./images/castleTown.png')}
+              alt="Hyrule map"
+              className={`map-item ${castleTown}`}
               id="castleTown">
             </img>
-            <img src={require('./images/deathMountain.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${mountain}`} 
+            <img src={require('./images/deathMountain.png')}
+              alt="Hyrule map"
+              className={`map-item ${mountain}`}
               id="mountain">
             </img>
-            <img src={require('./images/dekuTree.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${tree}`} 
+            <img src={require('./images/dekuTree.png')}
+              alt="Hyrule map"
+              className={`map-item ${tree}`}
               id="tree">
             </img>
-            <img src={require('./images/field.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${field}`} 
+            <img src={require('./images/field.png')}
+              alt="Hyrule map"
+              className={`map-item ${field}`}
               id="field">
             </img>
-            <img src={require('./images/gerudoValley.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${gerudo}`} 
+            <img src={require('./images/gerudoValley.png')}
+              alt="Hyrule map"
+              className={`map-item ${gerudo}`}
               id="gerudo">
             </img>
-            <img src={require('./images/hyruleCastle.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${castle}`} 
+            <img src={require('./images/hyruleCastle.png')}
+              alt="Hyrule map"
+              className={`map-item ${castle}`}
               id="castle">
             </img>
-            <img src={require('./images/kokiriForest.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${kokiri}`} 
+            <img src={require('./images/kokiriForest.png')}
+              alt="Hyrule map"
+              className={`map-item ${kokiri}`}
               id="kokiri">
             </img>
-            <img src={require('./images/kakariko.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${kakariko}`} 
+            <img src={require('./images/kakariko.png')}
+              alt="Hyrule map"
+              className={`map-item ${kakariko}`}
               id="kokiri">
             </img>
-            <img src={require('./images/lakeHylia.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${lake}`} 
+            <img src={require('./images/lakeHylia.png')}
+              alt="Hyrule map"
+              className={`map-item ${lake}`}
               id="lake">
             </img>
-            <img src={require('./images/lonLon.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${ranch}`} 
+            <img src={require('./images/lonLon.png')}
+              alt="Hyrule map"
+              className={`map-item ${ranch}`}
               id="ranch">
             </img>
-            <img src={require('./images/zorasDomain.png')} 
-              alt="Hyrule map" 
-              className={`map-item ${domain}`} 
+            <img src={require('./images/zorasDomain.png')}
+              alt="Hyrule map"
+              className={`map-item ${domain}`}
               id="domain">
             </img>
           </div>
@@ -121,8 +128,55 @@ const App = () => {
         </div>
       </div>
       <img src="hyrule" class="hyrule" alt="Hyrule map"></img>
-      </div>
+    </div>
   );
-}
 
-export default App;
+  function App(props) {
+    /* component state */
+    const [userState, setUserState] = useState({user: getUser()});
+
+  /* helper functions */
+  function handleSignupOrLogin() {
+    // place user into state using setter function
+    setUserState({user: getUser()});
+    //programmatically route user to dasboard
+    props.history.push('/');
+  }
+
+  function handleLogout() {
+    logout(); //this removes the token from localstorage
+    setUserState({user: null});
+    props.history.push('/');
+  }
+
+  return (
+    <div className="App">
+     <Header user={userState.user} handleLogout={handleLogout} />
+     <Switch>
+     {/* // inside switch you need a route for each page  */}
+       <Route exact path='/' render={() =>
+       <HomePage />
+       } />
+       <Route exact path='/dashboard' render={() =>
+        getUser() ? 
+          <DashboardPage />
+          :
+          <Redirect to="/login" />
+       } />
+      <Route exact path='/login' render={() =>
+       <SignupPage handleSignupOrLogin={handleSignupOrLogin} />
+       } />
+     <Route exact path='/signup' render={() =>
+        <LoginPage handleSignupOrLogin={handleSignupOrLogin} />
+       } />
+    
+   </Switch>
+    <Footer />
+    </div>
+  );
+      }
+    }
+
+
+  
+  export default withRouter(App);
--- src/fetching.js
+++ src/fetching.js
@@ -1,20 +1,21 @@
-export const fetchMe = () => {
 
-    
-    const getContent = () => {
-      let contentURL = 'https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=revisions&rvprop=content&format=json&titles='
-      fetch(contentURL + "Hyrule")
-      .then(function(resp) {
+export function fetchMe() {
+
+
+  function getContent() {
+    let contentURL = 'https://en.wikipedia.org/w/api.php?&origin=*&action=query&prop=revisions&rvprop=content&format=json&titles='
+    fetch(contentURL + "Hyrule")
+      .then(function (resp) {
         return resp.json()
-      }).then(function(data) {
+      }).then(function (data) {
         let page = data.query.pages
-        let pageId = Object.keys(data.query.pages)[0];
+        let pageId = Object.keys(data.query.pages)[0]
         console.log("HeyListen??", page[pageId].revisions[0]['*'])
       })
-    }
-  
-    getContent()
-    }
+  }
+
+  getContent()
+}
     
     
     
